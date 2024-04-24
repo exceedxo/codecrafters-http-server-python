90daa802af24eb379bf0d02bb917052f3ab7af39
Akira <nameless123xd@gmail.com>
2024-04-24 23:22:02 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -18,16 +18,11 @@ def main():
         path = parsed[1]
         if path == "/":
             conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        elif "/echo/" in path:
+            split_path = path.split("/echo/")
+            print(f"split path: {split_path}")
         else:
-            split_path = path.split("/")
-            is_echo = split_path[0] == "echo"
-            if not is_echo:
-                conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
-            string = split_path[1]
-            send_string = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent length: {len(string)}\r\n\r\n{string}"
-            print(f"Sending response: {send_string}")
-            encoded_string = send_string.encode()
-            conn.send(encoded_string)
+            conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
