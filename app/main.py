2f8204ed1048f51afa28969603586fcf1025d675
Akira <nameless123xd@gmail.com>
2024-04-24 18:27:32 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     while conn:
         receive = conn.recv(2048)
         decoded = receive.decode('utf-8')
-        splitted = receive.split()
+        splitted = decoded.split()
         if splitted[1] == "/":
             conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
         else:
