1a4cb43f7b23198b22f3e851e43f4123d00290b0
Akira <nameless123xd@gmail.com>
2024-04-25 00:01:01 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -10,22 +10,22 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server started")
     print("Waiting for client...")
-    (conn, address) = server_socket.accept() # wait for client
+    (conn, address) = server_socket.accept()
     print("Client connected.")
     while conn:
         receive = conn.recv(2048)
         parsed = decode_and_split(receive)
         path = parsed[1]
         if path == "/":
-            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
         elif "/echo/" in path:
             split_path = path.split("/echo/")
             string = split_path[1]
             send_string = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}"
             encoded_string = send_string.encode()
-            conn.sendall(encoded_string)
+            conn.send(encoded_string)
         else:
-            conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
