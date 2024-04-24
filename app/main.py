e6c28c7ab5663224638fc0d68f7894587bca2e02
Akira <nameless123xd@gmail.com>
2024-04-24 17:57:37 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -7,7 +7,11 @@ def main():
     print("Waiting for client...")
     (conn, address) = server_socket.accept() # wait for client
     print("Client connected")
-    conn.sendall("HTTP/1.1 200 OK\r\n\r\n")
+    conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    while conn:
+        receive = conn.recv(2048)
+        if not receive:
+            break
 
 if __name__ == "__main__":
     main()
