2faa2a45147060d1f5a3811b2cbde5a3d71174b8
Akira <nameless123xd@gmail.com>
2024-04-24 22:56:50 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 import socket
 
+def decode_and_split(bytes: bytes):
+    decoded = bytes.decode("utf-8")
+    splitted = decoded.split()
+    return splitted
+
 def main():
     print("Starting server...")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
@@ -9,9 +14,11 @@ def main():
     print("Client connected.")
     while conn:
         receive = conn.recv(2048)
-        decoded = receive.decode('utf-8')
-        splitted = decoded.split()
-        if splitted[1] == "/":
+        parsed = decode_and_split(receive)
+        path = parsed[1]
+        string = path.split("/")[-1]
+        print(f"string: {string}")
+        if parsed[1] == "/":
             conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
         else:
             conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
