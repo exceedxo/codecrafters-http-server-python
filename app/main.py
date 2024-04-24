483b86813ffdf9ed80a900477790e814cf1e9218
Akira <nameless123xd@gmail.com>
2024-04-24 18:09:48 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -7,10 +7,14 @@ def main():
     print("Waiting for client...")
     (conn, address) = server_socket.accept() # wait for client
     print("Client connected")
-    conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
     while conn:
         receive = conn.recv(2048)
-        print(receive)
+        receive.decode()
+        receive.split(" ")
+        if receive[1] == "/":
+            conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        else:
+            conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
