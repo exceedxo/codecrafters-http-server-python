31c1ddb6dea90379a981340f52535881266586c1
Akira <nameless123xd@gmail.com>
2024-04-24 23:51:09 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -15,18 +15,13 @@ def main():
     while conn:
         receive = conn.recv(2048)
         parsed = decode_and_split(receive)
-        print(f"[M] Parsed: {parsed}")
         path = parsed[1] if len(parsed) >= 1 else parsed[0]
-        print(f"[M] Path: {path}")
         if path == "/":
             conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
         elif "/echo/" in path:
             split_path = path.split("/echo/")
-            print(f"[M] Split Path: {split_path}")
             string = split_path[1]
-            print(f"[M] String: {string}")
-            send_string = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent length: {len(string)}\r\n\r\n{string}"
-            print(f"[M] Send String:\n{send_string}")
+            send_string = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}"
             encoded_string = send_string.encode()
             conn.sendall(encoded_string)
         else:
