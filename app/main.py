7a22859f79f5616e1fd961b1811933a1f27fd5ec
Akira <nameless123xd@gmail.com>
2024-04-24 23:59:18 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def main():
     while conn:
         receive = conn.recv(2048)
         parsed = decode_and_split(receive)
-        path = parsed[1] if len(parsed) >= 1 else parsed[0]
+        path = parsed[1]
         if path == "/":
             conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
         elif "/echo/" in path:
