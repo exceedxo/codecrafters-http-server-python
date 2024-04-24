625c5c2ad1720a7373969b016c2234e682fb09ca
Akira <nameless123xd@gmail.com>
2024-04-24 17:41:32 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server started")
     print("Waiting for client...")
-    server_socket.accept() # wait for client
+    (conn, address) = server_socket.accept() # wait for client
     print("Client connected")
-    server_socket.send("HTTP/1.1 200 OK\r\n\r\n")
+    conn.send("HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
