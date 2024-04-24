e545fc0a53fa3786cae8a064ed0cf2148fe8ed00
Akira <nameless123xd@gmail.com>
2024-04-24 23:17:12 +0200

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -15,7 +15,19 @@ def main():
     while conn:
         receive = conn.recv(2048)
         parsed = decode_and_split(receive)
-        print(parsed)
+        path = parsed[1]
+        if path == "/":
+            conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        else:
+            split_path = path.split("/")
+            is_echo = split_path[0] == "echo"
+            if not is_echo:
+                conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            string = split_path[1]
+            send_string = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent length: {len(string)}\r\n\r\n{string}"
+            print(f"Sending response: {send_string}")
+            encoded_string = send_string.encode()
+            conn.send(encoded_string)
 
 if __name__ == "__main__":
     main()
