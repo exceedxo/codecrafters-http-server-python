4300e276ff7113086eeaf823a8a0c38b8710418c
Akira <nameless123xd@gmail.com>
2024-04-25 14:52:55 +0200

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -9,27 +9,27 @@ async def decode_and_split(bytes: bytes):
 async def new_connection(conn: socket):
     print("Client connected.")
     while conn:
-        receive = await conn.recv(2048)
+        receive = conn.recv(2048)
         parsed = decode_and_split(receive)
         path = parsed[1]
         if path == "/":
-            await conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
         elif "/echo/" in path:
             split_path = path.split("/echo/")
             string = split_path[1]
             send_string = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}"
             encoded_string = send_string.encode()
-            await conn.sendall(encoded_string)
+            conn.sendall(encoded_string)
         elif path == "/user-agent":
             string = parsed[6]
             if string:
                 send_string = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}"
                 encoded_string = send_string.encode()
-                await conn.sendall(encoded_string) #
+                conn.sendall(encoded_string) #
             else:
-                await conn.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")        
+                conn.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")        
         else:
-            await conn.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")
+            conn.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")
 
 async def main():
     print("Starting server...")
